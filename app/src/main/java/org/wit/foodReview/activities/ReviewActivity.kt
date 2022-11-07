68aa576943753f4f08eca47a31bd55921813f46f
AnthonyLonergan <lonergan921@gmail.com>
2022-11-07 17:47:08 +0000

    Added Array.

--- app/src/main/java/org/wit/foodReview/activities/ReviewActivity.kt
+++ app/src/main/java/org/wit/foodReview/activities/ReviewActivity.kt
@@ -11,27 +11,26 @@ import timber.log.Timber.i
 class ReviewActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityReviewBinding
-
     var review = ReviewModel()
+    val reviews = ArrayList<ReviewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityReviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         Timber.plant(Timber.DebugTree())
-
         i("Review Activity started...")
 
         binding.btnAdd.setOnClickListener() {
             review.title = binding.reviewTitle.text.toString()
             if (review.title.isNotEmpty()) {
-                i("add Button Pressed: $review.title")
+                reviews.add(review.copy())
+                i("add Button Pressed: ${review}")
+                for (i in reviews.indices)
+                { i("Review[$i]:${this.reviews[i]}") }
             }
             else {
-                Snackbar
-                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                Snackbar.make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
