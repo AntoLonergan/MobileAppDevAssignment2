8cee0cc1e562112da2d565ef139c262a568cc514
AnthonyLonergan <lonergan921@gmail.com>
2022-11-07 10:56:04 +0000

    Added Event Handling.

--- app/src/main/java/org/wit/foodReview/ReviewActivity.kt
+++ app/src/main/java/org/wit/foodReview/ReviewActivity.kt
@@ -1,18 +1,36 @@
 package org.wit.foodReview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.wit.foodreview.R
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import org.wit.foodreview.databinding.ActivityReviewBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class ReviewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReviewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_review)
+
+        binding = ActivityReviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Timber.plant(Timber.DebugTree())
 
-        i("Food Review Activity started..")
+        i("Review Activity started...")
+
+        binding.btnAdd.setOnClickListener() {
+            val reviewTitle = binding.reviewTitle.text.toString()
+            if (reviewTitle.isNotEmpty()) {
+                i("add Button Pressed: $reviewTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
