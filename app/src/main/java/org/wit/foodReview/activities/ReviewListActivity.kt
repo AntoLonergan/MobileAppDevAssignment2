b378407af3d9ca7782a00af867c3037f600afcf9
AnthonyLonergan <lonergan921@gmail.com>
2022-11-07 20:51:29 +0000

    Added a Menu Button.

--- app/src/main/java/org/wit/foodReview/activities/ReviewListActivity.kt
+++ app/src/main/java/org/wit/foodReview/activities/ReviewListActivity.kt
@@ -3,11 +3,13 @@ package org.wit.foodReview.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import org.wit.foodReview.main.MainApp
 import org.wit.foodReview.models.ReviewModel
+import org.wit.foodreview.R
 import org.wit.foodreview.databinding.ActivityReviewListBinding
 import org.wit.foodreview.databinding.CardReviewBinding
 
@@ -20,6 +22,8 @@ class ReviewListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityReviewListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.toolbar.title = title
+        setSupportActionBar(binding.toolbar)
 
         app = application as MainApp
 
@@ -27,37 +31,42 @@ class ReviewListActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = ReviewAdapter(app.reviews)
     }
-}
 
-class ReviewAdapter constructor(private var reviews: List<ReviewModel>) :
-    RecyclerView.Adapter<ReviewAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardReviewBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-
-        return MainHolder(binding)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val review = reviews[holder.adapterPosition]
-        holder.bind(review)
-    }
+    class ReviewAdapter constructor(private var reviews: List<ReviewModel>) :
+        RecyclerView.Adapter<ReviewAdapter.MainHolder>() {
 
-    override fun getItemCount(): Int = reviews.size
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+            val binding = CardReviewBinding
+                .inflate(LayoutInflater.from(parent.context), parent, false)
 
-    class MainHolder(private val binding : CardReviewBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+            return MainHolder(binding)
+        }
 
-        fun bind(review: ReviewModel) {
-            binding.reviewName.text = review.name
-            binding.reviewAddress.text =  review.address
-            binding.reviewPostCode.text = review.postCode
-            binding.reviewJustEat.text = review.justEat
-            binding.reviewItems.text = review.items
-            binding.reviewPrice.text = review.price
-            binding.reviewComments.text = review.comments
-            binding.reviewRating.text = review.rating
+        override fun onBindViewHolder(holder: MainHolder, position: Int) {
+            val review = reviews[holder.adapterPosition]
+            holder.bind(review)
+        }
+
+        override fun getItemCount(): Int = reviews.size
+
+        class MainHolder(private val binding: CardReviewBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+
+            fun bind(review: ReviewModel) {
+                binding.reviewName.text = review.name
+                binding.reviewAddress.text = review.address
+                binding.reviewPostCode.text = review.postCode
+                binding.reviewJustEat.text = review.justEat
+                binding.reviewItems.text = review.items
+                binding.reviewPrice.text = review.price
+                binding.reviewComments.text = review.comments
+                binding.reviewRating.text = review.rating
+            }
         }
     }
 }
