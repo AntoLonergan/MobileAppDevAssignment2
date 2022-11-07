3a2ec07880e0f82c0020cdb2a4a795bebb53237a
AnthonyLonergan <lonergan921@gmail.com>
2022-11-07 22:45:17 +0000

    Refactored adapters.

--- app/src/main/java/org/wit/foodReview/activities/ReviewListActivity.kt
+++ app/src/main/java/org/wit/foodReview/activities/ReviewListActivity.kt
@@ -2,20 +2,16 @@ package org.wit.foodReview.activities
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import org.wit.foodReview.adapters.ReviewAdapter
 import org.wit.foodReview.main.MainApp
-import org.wit.foodReview.models.ReviewModel
 import org.wit.foodreview.R
 import org.wit.foodreview.databinding.ActivityReviewListBinding
-import org.wit.foodreview.databinding.CardReviewBinding
 
 
 class ReviewListActivity : AppCompatActivity() {
@@ -60,38 +56,4 @@ class ReviewListActivity : AppCompatActivity() {
                 notifyItemRangeChanged(0,app.reviews.size)
             }
         }
-
-
-    class ReviewAdapter constructor(private var reviews: List<ReviewModel>) :
-        RecyclerView.Adapter<ReviewAdapter.MainHolder>() {
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-            val binding = CardReviewBinding
-                .inflate(LayoutInflater.from(parent.context), parent, false)
-
-            return MainHolder(binding)
-        }
-
-        override fun onBindViewHolder(holder: MainHolder, position: Int) {
-            val review = reviews[holder.adapterPosition]
-            holder.bind(review)
-        }
-
-        override fun getItemCount(): Int = reviews.size
-
-        class MainHolder(private val binding: CardReviewBinding) :
-            RecyclerView.ViewHolder(binding.root) {
-
-            fun bind(review: ReviewModel) {
-                binding.reviewName.text = review.name
-                binding.reviewAddress.text = review.address
-                binding.reviewPostCode.text = review.postCode
-                binding.reviewJustEat.text = review.justEat
-                binding.reviewItems.text = review.items
-                binding.reviewPrice.text = review.price
-                binding.reviewComments.text = review.comments
-                binding.reviewRating.text = review.rating
-            }
-        }
-    }
 }
