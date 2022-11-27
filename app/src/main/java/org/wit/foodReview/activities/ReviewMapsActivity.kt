be30c9d8fb759f910da3184c60edc830aa8cde7a
AntoLonergan <lonergan921@gmail.com>
2022-11-27 22:38:49 +0000

    Marker Listener Added.

--- app/src/main/java/org/wit/foodReview/activities/ReviewMapsActivity.kt
+++ app/src/main/java/org/wit/foodReview/activities/ReviewMapsActivity.kt
@@ -3,20 +3,27 @@ package org.wit.foodReview.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import org.wit.foodReview.main.MainApp
 import org.wit.foodreview.databinding.ActivityReviewMapsBinding
 import org.wit.foodreview.databinding.ContentReviewMapsBinding
 
 
-class ReviewMapsActivity : AppCompatActivity() {
+class ReviewMapsActivity : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
 
     private lateinit var binding: ActivityReviewMapsBinding
     private lateinit var contentBinding: ContentReviewMapsBinding
     lateinit var map: GoogleMap
+    lateinit var app: MainApp
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        app = application as MainApp
 
         binding = ActivityReviewMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -54,6 +61,19 @@ class ReviewMapsActivity : AppCompatActivity() {
 
     private fun configureMap() {
         map.uiSettings.isZoomControlsEnabled = true
+        app.reviews.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.name).position(loc)
+            map.setOnMarkerClickListener(this)
+            map.addMarker(options)?.tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        contentBinding.currentName.text = marker.title
+
+        return false
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
